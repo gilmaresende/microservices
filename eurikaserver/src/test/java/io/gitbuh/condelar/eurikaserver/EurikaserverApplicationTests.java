2af11256e74d12cd2e8ef3180ffa5d1eac83f8ee
Gilmar <gilmaresende@gmail.com>
2023-10-13 15:49:08 -0300

    start eureka server

---
+++ eurikaserver/src/test/java/io/gitbuh/condelar/eurikaserver/EurikaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gitbuh.condelar.eurikaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurikaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
