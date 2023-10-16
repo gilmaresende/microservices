a8e3ff53150f5780c5a6432ceb908680d74ad215
Gilmar <gilmaresende@gmail.com>
2023-10-16 16:45:48 -0300

    created project mscloudgateway

---
+++ mscloudgateway/src/test/java/io/gitbuh/condelar/mscloudgateway/MscloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gitbuh.condelar.mscloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
