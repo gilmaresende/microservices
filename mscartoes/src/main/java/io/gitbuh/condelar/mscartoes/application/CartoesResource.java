010c928bad378ca4796860efc142455b7fae15e3
Gilmar <gilmaresende@gmail.com>
2023-10-24 14:25:28 -0300

    resorce cartao

--- mscartoes/src/main/java/io/gitbuh/condelar/mscartoes/application/CartoesResource.java
+++ mscartoes/src/main/java/io/gitbuh/condelar/mscartoes/application/CartoesResource.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("cartoes")
 @RequiredArgsConstructor
@@ -24,5 +26,11 @@ public class CartoesResource {
         Cartao cartao = request.toModel();
         service.save(cartao);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping(params = "renda")
+    public ResponseEntity<List<Cartao>> getCartoesRendaAte(@RequestParam("renda") Long renda){
+        List<Cartao> list = service.getCartoesrendaMenirIgual(renda);
+        return ResponseEntity.ok(list);
     }
 }
