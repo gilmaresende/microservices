aaff552c2abb6d44574b6eff407c49b6e3004676
Gilmar <gilmarende@gmail.com>
2024-05-24 21:21:53 -0300

    cliente service

--- msclientes/src/main/java/io/github/cursodsousa/msclientes/domain/Cliente.java
+++ msclientes/src/main/java/io/github/cursodsousa/msclientes/domain/Cliente.java
@@ -3,29 +3,63 @@ package io.github.cursodsousa.msclientes.domain;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
-@Data
-@NoArgsConstructor
 public class Cliente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
     private String cpf;
 
+    @Column
     private String nome;
 
+    @Column
     private Integer idade;
+
+
+    public Cliente() {
+    }
 
     public Cliente(String cpf, String nome, Integer idade) {
         this.cpf = cpf;
         this.nome = nome;
+        this.idade = idade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
         this.idade = idade;
     }
 }
---
+++ msclientes/src/main/java/io/github/cursodsousa/msclientes/servico/ClienteService.java
@@ -0,0 +1,28 @@
+package io.github.cursodsousa.msclientes.servico;
+
+import io.github.cursodsousa.msclientes.domain.Cliente;
+import io.github.cursodsousa.msclientes.repositories.ClienteRepository;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Transactional
+    public Cliente save(Cliente cliente) {
+        return this.clienteRepository.save(cliente);
+    }
+
+    public Optional<Cliente> findByCpf(String cpf){
+        return this.clienteRepository.findByCpf(cpf);
+    }
+}
